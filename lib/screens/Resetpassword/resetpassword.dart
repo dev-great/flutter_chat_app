849ad4cea19ba9b4804cea7adec677d93a508bee
Marshal Great <31458994+GR8T@users.noreply.github.com>
2021-07-22 23:56:02 +0100

    updated

---
+++ lib/screens/Resetpassword/resetpassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:our_village/screens/Resetpassword/components/body.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(
+        child: Column(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/fogottenpassword/fogottenpassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:our_village/screens/fogottenpassword/components/body.dart';
+
+class Fogottenpassword extends StatelessWidget {
+  const Fogottenpassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(
+        child: Column(),
+      ),
+    );
+  }
+}
